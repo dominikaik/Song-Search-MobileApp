21b667f792e694a6b6225cc78ebd893a8f22e22a
Dominika Iza Kowalska <dominiik@stud.ntnu.no>
2022-11-15 11:18:27 +0100

    Modified collapse element

--- frontend/components/SongList.tsx
+++ frontend/components/SongList.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import { useMutation, useQuery, useReactiveVar } from '@apollo/client';
-import { GET_SONGS } from "../GraphQL/Queries";
-import { RATE_SONG } from "../GraphQL/Mutations";
+import { GET_SONGS } from "../GraphQL/queries";
+import { RATE_SONG } from "../GraphQL/mutations";
 import { getSongsInputs, songsDataType } from "../types/songData";
 import { openSongTab, songCurrentPage, songQueryVars, songTotalPages } from '../GraphQL/cache';
 import { Text, View } from "react-native";
 import { DataTable} from 'react-native-paper';
-import { Collapse } from 'accordion-collapse-react-native';
+import { Collapse, CollapseHeader, CollapseBody} from 'accordion-collapse-react-native';
 import { Chip, VStack, HStack, IconButton } from "@react-native-material/core"; 
-import { Icon } from "react-native-vector-icons/Icon";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { Rating } from '@rneui/themed';
 
 export function SongList() {
@@ -50,9 +50,13 @@ export function SongList() {
           <DataTable.Cell key={"year"}> Year </DataTable.Cell>
         </DataTable.Row>
       </DataTable.Header>
+      
       <DataTable>
+        
             {data.getSongs.songs.map(((song, index: number) => (
               <>
+              <Collapse>
+          <CollapseHeader>
               <DataTable.Row key={song._id}>
                 {/* Inspiration from this video: https://www.youtube.com/watch?v=3v2cxwvWh80&t=688s */}
                 <DataTable.Cell>
@@ -70,40 +74,45 @@ export function SongList() {
                 <DataTable.Cell>{song.artists[0]}</DataTable.Cell>
                 <DataTable.Cell>{song.year}</DataTable.Cell>
               </DataTable.Row>
+              </CollapseHeader>
+              <CollapseBody>
               <DataTable.Row>
                 <DataTable.Cell>
-                  <Collapse in={open === index} timeout="auto" unmountOnExit>
-                  <View style={{width: "auto", display: "flex", flexWrap: 'wrap', flexDirection: 'column'}}> 
                   
-                  <Chip label="Info" color="primary"/>
-                  <HStack mt={2}>
-                  <Chip label={"Danceability: "+ (song.danceability * 100).toFixed()+"%"} variant="outlined" />
-                  <Chip label={"Popularity: "+ song.popularity + " / 100"} variant="outlined" />
-                  <Chip label={Math.floor(song.duration_ms / 60000) +" : "+ ((song.duration_ms % 60000) / 1000).toFixed(0) + " min"} variant="outlined" />
-                  {(song.explicit) ? (<Chip label={"Explicit"} variant="outlined" />) : (null)}
-                  </HStack>
+                    <View style={{width: "auto", display: "flex", flexWrap: 'wrap', flexDirection: 'column'}}> 
+                    
+                    <Chip label="Info" color="primary"/>
+                    <HStack mt={2}>
+                    <Chip label={"Danceability: "+ (song.danceability * 100).toFixed()+"%"} variant="outlined" />
+                    <Chip label={"Popularity: "+ song.popularity + " / 100"} variant="outlined" />
+                    <Chip label={Math.floor(song.duration_ms / 60000) +" : "+ ((song.duration_ms % 60000) / 1000).toFixed(0) + " min"} variant="outlined" />
+                    {(song.explicit) ? (<Chip label={"Explicit"} variant="outlined" />) : (null)}
+                    </HStack>
 
-                  <Chip label="Artists" color="primary"/>
-                  <HStack mt={2}>
-                  {song.artists.map((artist: string, i:number) => (
-                    <Chip key={i} label={artist} variant="outlined"/>
-                    ))}
-                  </HStack>
-              
-                  <VStack mt={2} spacing={2}>
-                  <Text>Rate this song:</Text>
-                  <Rating
-                      showRating
-                      type="song.rating"
-                      onStartRating={(newValue: number) => {rateSong({variables: {id: song._id, rating: newValue}})}}/>
-                  </VStack>
-                  </View>
-                  </Collapse>
+                    <Chip label="Artists" color="primary"/>
+                    <HStack mt={2}>
+                    {song.artists.map((artist: string, i:number) => (
+                      <Chip key={i} label={artist} variant="outlined"/>
+                      ))}
+                    </HStack>
+                
+                    <VStack mt={2} spacing={2}>
+                    <Text>Rate this song:</Text>
+                    <Rating
+                        showRating
+                        type="song.rating"
+                        onStartRating={(newValue: number) => {rateSong({variables: {id: song._id, rating: newValue}})}}/>
+                    </VStack>
+                    </View>
                 </DataTable.Cell>
                 </DataTable.Row>
+                </CollapseBody>
+                </Collapse>
                 </>
             )))}
+            
       </DataTable>
+
     </DataTable>
   </View>
   )
